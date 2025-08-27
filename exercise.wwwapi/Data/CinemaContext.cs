2274eddb4ef25bbbd3d428b2089ea37b05eb2e1c
Jonatan Berg Romundgard <jonatan.romundgard@no.experis.com>
2025-08-27 11:23:54 +0200

    Another auth project

---
+++ exercise.wwwapi/Data/CinemaContext.cs
@@ -0,0 +1,72 @@
+﻿using api_cinema_challenge.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+
+namespace api_cinema_challenge.Data
+{
+    public class CinemaContext : IdentityUserContext<ApplicationUser>
+    {
+        private string _connectionString;
+        public CinemaContext(DbContextOptions<CinemaContext> options) : base(options)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Customers
+            Customer customer = new Customer
+            {
+                Id = 1,
+                Name = "Jonatan",
+                Email = "jonnabr@hotmail.com",
+                Phone = "+4793277670",
+                CreatedAt = new DateTime(2025, 01, 20, 12, 00, 00, DateTimeKind.Utc),
+                UpdatedAt = new DateTime(2025, 01, 21, 15, 00, 00, DateTimeKind.Utc)
+            };
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                Name = "Isabel",
+                Email = "Isabel@hotmail.com",
+                Phone = "+4792088620",
+                CreatedAt = new DateTime(2025, 01, 20, 12, 00, 00, DateTimeKind.Utc),
+                UpdatedAt = new DateTime(2025, 01, 21, 15, 00, 00, DateTimeKind.Utc)
+            };
+
+            Customer customer3 = new Customer
+            {
+                Id = 3,
+                Name = "Marius",
+                Email = "marius@hotmail.com",
+                Phone = "+4798765432",
+                CreatedAt = new DateTime(2025, 01, 19, 12, 00, 00, DateTimeKind.Utc),
+                UpdatedAt = new DateTime(2025, 01, 21, 17, 00, 00, DateTimeKind.Utc)
+            };
+
+            Customer customer4 = new Customer
+            {
+                Id = 4,
+                Name = "Emma",
+                Email = "emma@hotmail.com",
+                Phone = "+4791234567",
+                CreatedAt = new DateTime(2025, 01, 18, 12, 00, 00, DateTimeKind.Utc),
+                UpdatedAt = new DateTime(2025, 01, 21, 18, 00, 00, DateTimeKind.Utc)
+            };
+
+            modelBuilder.Entity<Customer>().HasData([customer, customer2, customer3, customer4]);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ exercise.wwwapi/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using api_cinema_challenge.Models;
+using api_cinema_challenge.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_cinema_challenge.Repository
+{
+    public class Repository : IRepository
+    {
+        private CinemaContext _db;
+        public Repository(CinemaContext db)
+        {
+            _db = db;
+        }
+
+        // Customers
+        public async Task<Customer> GetCustomerById(int id)
+        {
+            return await _db.Customers.FindAsync(id);
+        }
+
+        public async Task<ICollection<Customer>> GetCustomers()
+        {
+            return await _db.Customers.ToListAsync();
+        }
+        
+        public async Task<Customer> CreateCustomer(Customer model)
+        {
+            await _db.AddAsync(model);
+            await _db.SaveChangesAsync();
+            return model;
+        }
+
+        public async Task<Customer> UpdateCustomer(int id, Customer model)
+        {
+            var entity = await GetCustomerById(id);
+            entity = model;
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Customer> DeleteCustomer(int id)
+        {
+            var entity = await GetCustomerById(id);
+            if (entity == null) return entity;
+            _db.Customers.Remove(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
